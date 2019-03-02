154f05486a8b5928ef4eb3a1e09a57dd90d19b04
Laksh Singla <lakshsingla@gmail.com>
2019-03-02 22:45:53 +0530

    Changed queue methods

--- lab3/queue.c
+++ lab3/queue.c
@@ -31,18 +31,32 @@ void insert_in_queue(queue *q, long long int data) {
 	q->tail = new_node;
 }
 
-void deque(queue *q) {
+long long int peek(queue *q) {
 	if(q == NULL) {
 		printf("Enter a Queue pointer\n");	
-		return;
+		return -1;
 	}
 	if(is_queue_empty(q)) {
 		printf("Queue underflow, cannot remove any more elements\n");
-		return;
+		return -1;
+	}
+	return q->head->data;
+}
+
+long long int deque(queue *q) {
+	if(q == NULL) {
+		printf("Enter a Queue pointer\n");	
+		return -1;
+	}
+	if(is_queue_empty(q)) {
+		printf("Queue underflow, cannot remove any more elements\n");
+		return -1;
 	}
 	node *elem = q->head;
 	q->head = elem->next;
+	long long int val = elem->data;
 	free(elem);
+	return val;
 }
 
 void print_queue(queue *q) {
@@ -62,4 +76,15 @@ bool is_queue_empty(queue *q) {
 	if(q == NULL)
 		printf("Enter a Queue pointer\n");	
 	return (q->head == NULL);
+}
+
+long long int len_queue(queue* q) {
+	if(q == NULL){
+		printf("Enter a Queue pointer\n");	
+		return -1;
+	}
+	long long int len = 0;
+	node *head = q->head;
+	while(head != NULL && ++len) head = head->next;
+	return len;
 }
--- lab3/queue.h
+++ lab3/queue.h
@@ -7,8 +7,10 @@
 
 queue create_queue();
 void insert_in_queue(queue*, long long int);
-void deque(queue *);
+long long int peek(queue*);
+long long int deque(queue *);
 void print_queue(queue *);
 bool is_queue_empty(queue*);
+long long int len_queue(queue*);
 
 #endif
