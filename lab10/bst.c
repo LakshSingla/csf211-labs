e530d054f17f0fac00d63e14080308dd60848639
Laksh Singla <lakshsingla@gmail.com>
2019-04-10 11:39:45 +0530

    Helper functions added

--- lab10/bst.c
+++ lab10/bst.c
@@ -13,19 +13,26 @@ node* create_node() {
 	return new_node;
 }
 
-node* create_new_tree(int value) {
+/*node* create_new_tree(int value) {
 	node *new_node = create_node();
 	new_node->value = value;
 	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->hb = 0;
+	new_node->right = NULL; new_node->hb = 0;
 	return new_node;
-}
+}*/
 
-node *add_value(int value) {
+node *add_value(node* tree, int value) {
 	node *new_node = create_node();	
-	while(true) {
-			
+	new_node->value = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->hb = 0;ew_node.
+	//In case, no tree is present, create a new tree and return it;
+	if(tree == NULL) return new_node;
+
+	node *prev = tree;
+	while(true){
+		
 	}
 }
 
@@ -34,4 +41,21 @@ void inorder_traversal(node *tree) {
 	inorder_traversal(tree->left);
 	printf("%d ", tree->value);
 	inorder_traversal(tree->right);
+}
+
+node* find_parent(node *tree, node *child) {
+	//Empty tree or tree is the parent node
+	if(tree == NULL || tree == child) return NULL;
+	else if(tree->left == child || tree->right == child) return tree;
+
+	if(tree->value > child->value) return find_parent(tree->left, child);
+	else if(tree->value < child->value) return find_parent(tree->right, child);
+	//In case of multiple keys, one may need to search for both
+	//left and right subtrees, thus need to backtrack
+	else {
+		node *maybe_left_child;
+		maybe_left_child = find_parent(tree->left, child);
+		if(maybe_left_child) return maybe_left_child;
+		else return find_parent(tree->right, child);
+	}
 }
--- lab10/bst.h
+++ lab10/bst.h
@@ -5,9 +5,10 @@
 
 node* create_node();
 node* create_new_tree(int);
-node* add_value(int);
+node* add_value(node*, int);
 node* find_value(int);
 node* delete_value(int);
+node* find_parent(node*, node*);
 void inorder_traversal(node*);
 
 #endif
